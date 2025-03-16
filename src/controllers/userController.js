9b696de2f2387dcb24ab4532b88036cb9236fd77
reimunyancat <lasco647@gmail.com>
2025-03-16 16:42:48 +0900

    1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,3 @@
-const join = (req, res) => res.send("Join")
+export const join = (req, res) => res.send("Join");
+export const edit = (req, res) => res.send("edit");
+export const remove = (req, res) => res.send("remove");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,1 +1,3 @@
-const trending = (req, res) => res.send("Home Page Videos")
+export const trending = (req, res) => res.send("Home Page Videos")
+export const watch = (req, res) => res.send("watch")
+export const edit = (req, res) => res.send("edit")
