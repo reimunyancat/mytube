d07af82567909f0aef07cd926821411ddd34e963
reimunyancat <lasco647@gmail.com>
2025-05-08 17:22:50 +0900

    today

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,29 @@
+let videos = [
+    {
+        title: "first video",
+        rating: 5,
+        comments: 2,
+        createat: "1 second ago",
+        views: 1,
+        id: 1,
+    },
+    {
+        title: "second video",
+        rating: 5,
+        comments: 5,
+        createat: "1 minute ago",
+        views: 1974,
+        id: 2,
+    },
+]
+
+
 export const trending = (req, res) => res.render("home", {pageTitle: "Home"})
-export const see = (req, res) => res.render("watch")
+export const watch = (req, res) => {
+    const { id } = req.parms;
+    const video = videos[id-1];
+    return res.render("watch", { pageTitle: `Watching ${video.title}`, video});
+}
 export const edit = (req, res) => res.send("edit")
 export const search = (req, res) => res.send("search")
 export const upload = (req, res) => res.send("upload")
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {see, edit, upload, deleteVideo} from "../controllers/videoController"
+import {watch, edit, upload, deleteVideo} from "../controllers/videoController"
 
 const videoRouter = express.Router()
 
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
