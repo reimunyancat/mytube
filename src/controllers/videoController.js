4d8398a0441cce388e2a91ead8016c6e20a6a4cc
reimunyancat <lasco647@gmail.com>
2025-05-13 07:12:10 +0900

    muhehe

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -15,20 +15,26 @@ let videos = [
         views: 1974,
         id: 2,
     },
-]
+];
 
 
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", videos})
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", videos });
 export const watch = (req, res) => {
     const { id } = req.params;
     const video = videos[id-1];
-    return res.render("watch", { pageTitle: `Watching: ${video.title}`, video});
-}
-export const edit = (req, res) => {
+    return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
     const { id } = req.params;
     const video = videos[id-1];
-    return res.render("edit", { pageTitle: `Editing: ${video.title}` })
+    return res.render("edit", { pageTitle: `Editing: ${video.title}`, video })
+};
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    video[id-1].title = title;
+    return res.redirect(`/videos/${id}`);
 }
-export const search = (req, res) => res.send("search")
-export const upload = (req, res) => res.send("upload")
-export const deleteVideo = (req, res) => res.send("delete video")
+export const search = (req, res) => res.send("search");
+export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => res.send("delete video");
--- src/server.js
+++ src/server.js
@@ -7,15 +7,16 @@ import userRouter from "./routers/userRouter";
 const PORT = 4000;
 
 const app = express();
-const logger = morgan("dev")
+const logger = morgan("dev");
 
-app.set("view engine", "pug")
-app.set("views", process.cwd() + "/src/views")
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 
-app.use(logger)
-app.use("/", globalRouter)
-app.use("/videos", videoRouter)
-app.use("/users", userRouter)
+app.use(logger);
+app.use(express.urlencoded({ extended:true }));
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => 
     console.log(`✅Server listening on port http://localhost:${PORT}🚀`); 
