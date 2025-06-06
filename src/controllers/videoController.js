602bb2118c5cada316dad05adeb895b3d571b164
reimunyancat <lasco647@gmail.com>
2025-06-07 01:11:26 +0900

    wow

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,37 +1,49 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-    try {
-        const videos = await Video.find({});
-        console.log("videos", videos);
-        res.render("home", { pageTitle: "Home", videos });
-    } catch (error) {
-        console.log("errors", error);
-        res.render("home", { pageTitle: "Home", videos: [] });
-    }
-}
+  try {
+    const videos = await Video.find({});
+    console.log("videos", videos);
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log("errors", error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
+};
 export const watch = (req, res) => {
-    const { id } = req.params;
-    return res.render("watch", { pageTitle: `Watching: ${video.title}` });
+  const { id } = req.params;
+  return res.render("watch", { pageTitle: `Watching: ${video.title}` });
 };
 export const getEdit = (req, res) => {
-    const { id } = req.params;
-    return res.render("edit", { pageTitle: `Editing: ${video.title}` })
+  const { id } = req.params;
+  return res.render("edit", { pageTitle: `Editing: ${video.title}` });
 };
 export const postEdit = (req, res) => {
-    const { id } = req.params;
-    const { title } = req.body;
-    return res.redirect(`/videos/${id}`);
-}
+  const { id } = req.params;
+  const { title } = req.body;
+  return res.redirect(`/videos/${id}`);
+};
 export const search = (req, res) => res.send("search");
 export const deleteVideo = (req, res) => res.send("delete video");
 
 export const getUpload = (req, res) => {
-    return res.render("upload", { pageTitle: "Upload Video" });
-}
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
 
-export const postUpload = (req, res) => {
-    const { title } = req.body;
-
-    return res.redirect("/");
-}
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((w) => `#${w}`),
+    });
+  } catch (error) {
+    console.log("video upload error", error);
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error.message,
+    });
+  }
+  return res.redirect("/");
+};
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    createdAt: Date,
-    hashtags: [{ type: String }],
-    meta: {
-        views: Number,
-        rating: Number,
-    }
+  title: { type: String, required: true, trim: true, maxLength: 100 },
+  description: { type: String, required: true, trim: true },
+  createdAt: { type: Date, default: Date.now },
+  hashtags: [{ type: String, trim: true }],
+  meta: {
+    views: { type: Number, default: 0 },
+    rating: { type: Number, default: 0 },
+  },
 });
 
 const Video = mongoose.model("Video", videoSchema);
