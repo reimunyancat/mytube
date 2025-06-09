e6e39f8d22f41f25a95f666bc641c4ac1cf57d69
reimunyancat <lasco647@gmail.com>
2025-06-10 07:06:34 +0900

    1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,19 +12,31 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  return res.render("watch", {
-    pageTitle: video.title,
-    video,
-  });
+  if (video) return res.render("watch", { pageTitle: video.title, video });
+  return res.render("404", { pageTitle: "Video not found." });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing: ${video.title}` });
+  const video = await Video.findById(id);
+  if (video)
+    return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
+  return res.render("404", { pageTitle: "Video not found." });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
-  return res.redirect(`/videos/${id}`);
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (video) {
+    await Video.findByIdAndUpdate(id, {
+      title,
+      description,
+      hashtags: hashtags
+        .split(",")
+        .map((w) => (w.startsWith("#") ? w : `#${w}`)),
+    });
+    return res.redirect(`/videos/${id}`);
+  }
+  return res.render("404", { pageTitle: "Video not found." });
 };
 export const search = (req, res) => res.send("search");
 export const deleteVideo = (req, res) => res.send("delete video");
@@ -39,7 +51,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((w) => `#${w}`),
+      hashtags: hashtags.split(",").map(w.startsWith("#") ? w : `#${w}`),
     });
   } catch (error) {
     console.log("video upload error", error);
