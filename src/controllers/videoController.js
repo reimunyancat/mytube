664aa5cff4768d94ff757a7f3c06e9730e52d4e2
reimunyancat <lasco647@gmail.com>
2025-06-13 07:09:34 +0900

    =)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,7 +30,7 @@ export const postEdit = async (req, res) => {
     await Video.findByIdAndUpdate(id, {
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect(`/videos/${id}`);
   }
@@ -49,7 +49,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map(w.startsWith("#") ? w : `#${w}`),
+      hashtags: Video.formatHashtags(hashtags),
     });
   } catch (error) {
     console.log("video upload error", error);
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+export const formatHashtags = (hashtags) =>
+  hashtags.split(",").map((w) => (w.startsWith("#") ? w : `#${w}`));
+
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 100 },
   description: { type: String, required: true, trim: true },
@@ -11,10 +14,8 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
-videoSchema.pre("save", async function () {
-  this.hashtags = this.hashtags[0]
-    .split(",")
-    .map((w) => (w.startsWith("#") ? w : `#${w}`));
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags.split(",").map((w) => (w.startsWith("#") ? w : `#${w}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
