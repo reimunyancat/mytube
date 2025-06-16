c4dd0ae1ac944e8b4ca1a7888464317ef8126029
reimunyancat <lasco647@gmail.com>
2025-06-16 16:36:17 +0900

    search

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -60,8 +60,16 @@ export const deleteVideo = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const { keyword } = req.query;
-  console.log(keyword);
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}`, "i"),
+      },
+    });
+    return res.render("search", { pageTitle: "search", videos });
+  }
   return res.render("search", { pageTitle: "search" });
 };
