259e10b97c8ecfe6d3329dc9b39b4b8557d0f0e3
reimunyancat <lasco647@gmail.com>
2025-06-19 16:39:36 +0900

    d

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/wetube")
+  .connect("mongodb://127.0.0.1:27017/mytube")
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((error) => console.log("❌ DB Error", error));
 
