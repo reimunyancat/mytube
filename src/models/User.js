0243b6313d5ea8a265d0157141dfb5e2ad175d25
reimunyancat <lasco647@gmail.com>
2025-06-20 07:23:41 +0900

    password

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,16 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  user: { type: String, required: true },
+  name: { type: String, required: true },
   location: String,
+});
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 const User = mongoose.model("User", userSchema);
