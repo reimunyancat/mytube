2bda81c5c5055dd379b23ac29fba25704c6aae9d
reimunyancat <lasco647@gmail.com>
2025-07-21 10:45:50 +0900

    d

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import "./db";
 import "./models/Video";
 import "./models/User";
@@ -6,6 +7,6 @@ import app from "./server";
 const PORT = 4000;
 
 const handleListening = () =>
-  console.log(`✅Server listening on port http://localhost:${PORT}🚀`);
+  console.log(`✅Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
--- src/server.js
+++ src/server.js
@@ -1,14 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
-import dotenv from "dotenv";
 import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import { localsMiddleware } from "./middlewares";
-
-dotenv.config();
 
 const app = express();
 const logger = morgan("dev");
