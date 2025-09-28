caac76c23aa8a200e9755ed24e7ec4df04aefd1e
reimunyancat <lasco647@gmail.com>
2025-09-29 07:04:30 +0900

    a

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import User from "../models/User";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
@@ -8,8 +9,9 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  const owner = await User.findById(video.owner);
   if (video)
-    return res.render("videos/watch", { pageTitle: video.title, video });
+    return res.render("videos/watch", { pageTitle: video.title, video, owner });
   return res.status(404).render("404", { pageTitle: "Video not found." });
 };
 
@@ -44,6 +46,9 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
+  const {
+    user: { _id },
+  } = req.session;
   const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
@@ -52,6 +57,7 @@ export const postUpload = async (req, res) => {
       description,
       fileUrl,
       hashtags: Video.formatHashtags(hashtags),
+      owner: _id,
     });
   } catch (error) {
     console.log("video upload error", error);
